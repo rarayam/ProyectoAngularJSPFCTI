2c0df766d741d03d06ab0e3b537e54b8b56a9413
Roger Araya <roger.araya@pfcti.com>
2018-01-29 16:12:21 -0600

    Subida inicial

---
+++ app/controllers/UserController.js
@@ -0,0 +1,21 @@
+﻿app.controller('UserController', ['$scope','DataUsers',
+    function ($scope, DataUsers) {
+        $scope.user = {};
+
+        $scope.users = DataUsers.getAll();
+
+        $scope.save = function () {
+            //Se inserta el contenido de user que fue seteado en la vista
+            DataUsers.Insert($scope.user);
+            // se limpia el formulario
+            $scope.user = {};
+            //Se cierra la ventana de modal
+            angular.element('#exampleModal').modal('hide');
+
+        }
+
+        $scope.remove = function (id) {
+            DataUsers.remove(id);
+        }
+    }
+])
