fd2d4f666012bce83397833989368a6f83e1a340
Roger Araya <roger.araya@pfcti.com>
2018-02-14 15:50:16 -0600

    Se agrega uso de servicio de login con AD desde el site

--- FronEntd/app/controllers/login-controller.js
+++ FronEntd/app/controllers/login-controller.js
@@ -1,6 +1,11 @@
 ﻿app.controller('LoginController', ['$scope', '$http', 'Config',
     function ($scope, $http, Config) {
-        $scope.user = {};
+        $scope.user = {
+            "UserName": "",
+            "Password": "",
+            "SuccessAuthentication": false
+        };
+
         localStorage.removeItem('token');
 
         $scope.login = function (form) {
@@ -10,26 +15,27 @@
                 
             }
 
-
-            //esto se debe reemplazar por la validacion contra AD
-            localStorage.setItem('token', 'autenticado');
-            window.location = '#!/';
-            /*$http.post(Config.HostServices + '/api/Account/Login',
+            $http.post(Config.HostServices + '/api/Login',
                 $scope.user)
                 .then(function (response) {
                     // On Sucess
-                    if (response && (response.status == 204 || response.status == 200) && response.data) {
-                        localStorage.setItem('token', response.data);
-                        window.location = '#!/';
+                    if (response && (response.status == 201 || response.status == 200) && response.data) {
+                        var result = response.data;
+                        if (result.SuccessAuthentication) {
+                            localStorage.setItem('token', response.UserName);
+                            window.location = '#!/';
+                        } else {
+                            alert('Usuario o password inválido');
+                        }                        
                     }
                     else {
-                        alert('Email or password wrongsdfsdfsdfs!');
+                        alert('No es posible autenticarse en este momento');
                     }
                 }, function (response) {
                     // On Error
-                    console.error('Login Service Error!!!');
-                });
-            */
+                    alert('No es posible acceder al servicio de autenticación en este momento');
+                    console.error(response.data);
+                });            
         }
     }
 ]);
