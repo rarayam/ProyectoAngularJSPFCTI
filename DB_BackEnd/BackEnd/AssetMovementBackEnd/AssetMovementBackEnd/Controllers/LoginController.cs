18f82949978cfeadd062277a403517a0c0c9a092
Roger Araya <roger.araya@pfcti.com>
2018-02-14 13:35:48 -0600

    Se agrega controlador de web api para login contra active directory

---
+++ DB_BackEnd/BackEnd/AssetMovementBackEnd/AssetMovementBackEnd/Controllers/LoginController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using AssetMovementBackEnd.Models;
+using System.Security.Principal;
+using System.DirectoryServices.Protocols;
+
+namespace AssetMovementBackEnd.Controllers
+{
+
+   public class UserAuthentication {
+        public string UserName { get; set; }        
+        public bool SuccessAuthentication { get; set; }
+
+        public UserAuthentication(string pUserName, bool pSuccessAuthentication) {
+            UserName = pUserName;
+            SuccessAuthentication = pSuccessAuthentication;
+        }
+
+    }
+
+    public class LoginController : ApiController
+    {
+       
+
+        // GET: api/Login
+        [ResponseType(typeof(UserAuthentication))]
+        public IHttpActionResult GetLogin(string UserName, string Password)
+        {
+            UserAuthentication userAuth = new UserAuthentication(UserName, false);
+            try
+            {
+                LdapConnection connection = new LdapConnection("tipfc.com");
+                NetworkCredential credential = new NetworkCredential(UserName, Password);
+                connection.Credential = credential;
+                connection.Bind();
+                userAuth.SuccessAuthentication = true;
+            }
+            catch (LdapException)
+            {
+                userAuth.SuccessAuthentication = false;
+            
+            }
+            catch (Exception)
+            {
+                userAuth.SuccessAuthentication = true;
+            
+            }            
+
+            return Ok(userAuth);
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+           
+            base.Dispose(disposing);
+        }
+
+       
+    }
+}
