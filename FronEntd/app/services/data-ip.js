2528169c4213c342b8f95e2b2d12189795d075de
Roger Araya <roger.araya@pfcti.com>
2018-02-17 16:12:51 -0600

    Cambios en servicio

--- FronEntd/app/services/data-ip.js
+++ FronEntd/app/services/data-ip.js
@@ -1,42 +1,21 @@
-﻿app.service('DataEmployee', [
-    function () {
-        var count = 0;
-        var EmployeeCollection = [
-        ];
-
-        this.getAll = function () {
-            return EmployeeCollection;
-        }
-
-        this.getHome = function () {
-            var userhome = [];
-            for (var i = 0; i < EmployeeCollection.length; i++) {
-                var uHome = {
-                    NombreCompleto: EmployeeCollection[i].Nombre + ' ' + EmployeeCollection[i].Apellido,
-                    Edad: Employee[i].Edad
-                            };
-                userhome.push(uHome);
-            }
-            return userhome;
-        }
-
-        this.Insert = function (Employee) {
-            var u = {
-                Name: Employee.Name,
-                Id: Employee.Id,
-                IP: Employee.IP,
-                ServiceUnit: Employee.ServiceUnit,
-                ResourceId: Employee.ResourceId
+﻿app.service('IpData', ['$http',
+    function ($http) {
+        this.GetIp = function () {
+            var Ip = 'No se pudo obtener, verifique conexión de red';
+            $http.get(Config.HostServices + '/api/Ip').then(
+                function (response) {
+                    //en caso exitoso
+                    if (response.status == 200 && response.data) {
+                        var Ip = response.data;
+                    }
+                }
+            ), function (response) {
+                //en caso de error
+                alert('Error al consultar IP!');
             }
 
-
-            EmployeeCollection.push(u);
         }
 
-        this.Remove = function (id, Employee) {
-            
-        }
 
-        
     }
 ]);
